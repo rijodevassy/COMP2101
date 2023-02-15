848e37d942890861e3c7898a769bcad99c46183e
Rijo Devassy <rijocd@gmail.com>
2023-02-14 21:15:12 -0500

    created bash file

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,25 @@
 #sysinfo.sh#File stored on the location COMP2101/bash
 
-#To show the FQDN 
-echo FQDN: $(hostname --fqdn)
+#To assign the FQDN 
+fqdn=$( hostname --fqdn ) 
 
-#To show the OS details on the system
-os=$(hostnamectl)
-echo Operatingsystem and Version :
-echo "$os"
+#To assign the OS details on the system
+os=$( hostnamectl|grep Operating )
 
-#To show the IP address
-echo IP address: $(hostname --all-ip-addresses)
+#To assign the IP address
+IP=$( hostname -I )
 
-#To show the storage file of system
-echo Free Storage on rootfilesystem: 
-df -h | grep Filesystem
-df -h | grep /dev/sda3 
+#To assign the free storage file of system
+
+root=$( df --output=avail --block-size=G / | awk 'NR==2 {print $1}' )
+cat<<EOF
+Details for $fqdn
+###########################
+Fully Qualified Domain Name: $fqdn
+Operation system and version info: $os
+IP address: $IP
+Root file free storage: $root
+##########################
+EOF
+
+
